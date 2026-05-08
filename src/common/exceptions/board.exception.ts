d4207effe46bc472b314dc2df04e514a4d72d5a1
jinwoo-kristin <jinu22@kristinkorea.com>
2026-05-08 09:34:08 +0900

    reivew위한 파일 삭제

--- src/common/exceptions/board.exception.ts
+++
@@ -1,8 +0,0 @@
-import { HttpException } from '@nestjs/common';
-import { ErrorCode } from './error-code';
-
-export class BoardException extends HttpException {
-  constructor(readonly errorCode: ErrorCode) {
-    super(errorCode.message, errorCode.status);
-  }
-}
